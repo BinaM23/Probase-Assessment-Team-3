305316925d3be30ccd22ffecc81296653073874d
chisha428 <chishaonesimus@gmail.com>
2024-04-06 12:33:21 +0200

    com 2

--- baseApp/views.py
+++ baseApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 from .models import Payment
+from django.http import HttpResponseBadRequest
+
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -15,6 +19,26 @@ def paymentSuccess(request):
         expirydate = request.POST.get('expirydate')
         cvc = request.POST.get('cvc')
         amount = request.POST.get('amount')
+
+        # Validate input
+        if not cardholder or not cardnumber or not expirydate or not cvc or not amount:
+            return HttpResponseBadRequest('Missing required fields')
+
+        if not cardnumber.isdigit() or len(cardnumber) != 16:
+            return HttpResponseBadRequest('Invalid card number, card number should be 16 characters long')
+
+        if len(expirydate) != 5 or not expirydate[:2].isdigit() or not expirydate[3:].isdigit() or expirydate[2] != '/':
+            return HttpResponseBadRequest('Invalid expiry date format')
+
+        if not cvc.isdigit() or len(cvc) != 3:
+            return HttpResponseBadRequest('Invalid CVC, cvv/cvc should be 3 charactes long')
+
+        try:
+            amount = float(amount)
+            if amount <= 0:
+                raise ValueError
+        except ValueError:
+            return HttpResponseBadRequest('Invalid amount')
 
         # Create a new Payment object and save it to the database
         payment = Payment.objects.create(
